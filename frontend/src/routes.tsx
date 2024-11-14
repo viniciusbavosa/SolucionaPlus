93fd983d6fa86849fef1f7dcf81d8f08752bb392
viniciusbavosa <viniciusbavosa@outlook.com>
2024-11-14 19:21:30 -0300

    feat: set up routes

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages";
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <h1>Erro</h1>,
+  }
+]); 
